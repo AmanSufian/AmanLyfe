eb5177ae9736385205fe9d440b936b1cd468b39a
Aman Sufian <amansufian04@gmail.com>
2024-11-05 23:29:38 +0800

    project 1
    learn how to use github , visual code

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int num;
+double dec;
+char a;
+char b [20];
+cout << " Enter a decimal number:";
+cin>> num;
+cout <<"Enter a decimal number:";
+cin >> dec;
+cout<< " Enter a single character: ";
+cin >> a;
+cout <<" Enter an array of charaacters:";
+cin>> b;
+}
